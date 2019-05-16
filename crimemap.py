c2e115103f91981dcb646d12500b5b262622bf7e
carlos <carlos@pucgoias.edu.br>
2019-05-16 09:59:42 -0300

    New functionality

--- crimemap.py
+++ crimemap.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
 
 import dbconfig
 if dbconfig.test:
@@ -14,12 +15,17 @@ DB = DBHelper()
 
 @app.route("/")
 def home():
+    crimes = DB.get_all_crimes()
+    crimes = json.dumps(crimes)
+    return render_template("home.html", crimes=crimes)
+    """
     try:
         data = DB.get_all_inputs()
     except Exception as e:
         print (e) 
         data = None
     return render_template("home.html", data=data)
+    """
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -36,6 +42,18 @@ def clear():
         DB.clear_all()
     except Exception as e:
         print (e) 
+    return home()
+
+@app.route("/submitcrime", methods=['POST'])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+
+    DB.add_crime(category, date, latitude, longitude, description)
+
     return home()
 
 if __name__ == '__main__':
--- dbhelper.py
+++ dbhelper.py
@@ -1,5 +1,6 @@
 import pymysql
 import dbconfig
+import datetime
 
 class DBHelper():
     def connect(self, database="crimemap"):
@@ -8,14 +9,24 @@ class DBHelper():
         password=dbconfig.db_password,
         db=dbconfig.db_database)
 
-    def get_all_inputs(self):
+    def get_all_crimes(self):
         connection = self.connect()
 
         try:
-            query = "SELECT description FROM crimes;"
+            query = "SELECT latitude, longitude, date, category, description FROM crimes;"
             with connection.cursor() as cursor:
                 cursor.execute(query)
-            return cursor.fetchall()
+            named_crimes = []
+            for crime in cursor:
+                named_crime = {
+                    'latitude': crime[0],
+                    'longitude': crime[1],
+                    'date':  datetime.datetime.strftime(crime[2], '%Y-%m-%d'),
+                    'category': crime[3],
+                    'description': crime[4]
+                }
+                named_crimes.append(named_crime)
+            return named_crimes
         finally:
             connection.close()
     
@@ -32,14 +43,27 @@ class DBHelper():
         finally:
             connection.close()
 
-        def clear_all(self):
-            connection = self.connect()
+    def clear_all(self):
+        connection = self.connect()
 
-            try:
-                query = "DELETE FROM crimes;"
-                with connection.cursor() as cursor:
+        try:
+            query = "DELETE FROM crimes;"
+            with connection.cursor() as cursor:
                     cursor.execute(query)
                     connection.commit()
-            finally:
-                connection.close()
+        finally:
+            connection.close()
+
+    def add_crime(self, category, date, latitude, longitude, description):        
+        connection = self.connect()
+        try:
+            query = "INSERT INTO crimes (category, date, latitude, longitude, description) VALUES (%s, %s, %s, %s, %s)"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (category, date, latitude, longitude, description))
+                connection.commit()
+        except Exception as e:
+                print(e)
+        finally:
+            connection.close()
+        
 
